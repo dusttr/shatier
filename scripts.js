92a57458752b732b7c1b2732633f976db400ac15
dusttr <121070081+dusttr@users.noreply.github.com>
2022-12-21 20:49:16 -0500

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,28 @@
+//Auto slideshow
+var index = 0;
+carousel();
+
+function carousel() {
+var i;
+var x = document.getElementsByClassName("slides");
+
+for (i = 0; i < x.length; i++) {
+	x[i].style.display = "none";  
+}
+index++;
+if (index > x.length) {index = 1}    
+	x[index-1].style.display = "block";  
+	setTimeout(carousel, 6000);
+}
+
+
+
+//Smooth scrolling anchor links
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+	anchor.addEventListener("click", function(e){
+		e.preventDefault();
+		document.querySelector(this.getAttribute("href")).scrollIntoView({
+			behavior : "smooth"
+		});
+	});
+});
